cbcfc814fd193786c5d1fb5aa7d969ee176a7a03
srwr-tech <srwr.tech@gmail.com>
2022-10-01 15:54:07 +0530

    first commit

---
+++ Basic.js
@@ -0,0 +1,350 @@
+// Trim
+
+                // let str= "   Sarwar    "
+                // let newTrim=str.trim();
+                // console.log(newTrim.length);
+                
+    
+//to Upper Case
+
+                // console.log(str.toUpperCase());
+
+//to lower case
+    
+                // console.log(str.toLowerCase());
+
+//Slice
+                // it will use to remove element from array .slice(2,4) it means first element is indicate starting of removed elemet and second element indicate till that element will remove
+    
+                //  let arr=["mango","apple","banana","pineapple"]
+                // console.log(arr.slice(1,3));
+                // let bbw=arr.substring(1,3)
+                // console.log(bbw);
+
+// Splice
+
+                // let arr1=["sonu","monu","titu","pillu"]
+                // console.log(arr1.splice(1,3))
+
+//concat
+                    // let str="sarwar"
+                    // let arr="alam"
+
+                    // console.log(str.concat(" " +arr))
+                    // console.log(str+ " "+arr);
+
+
+// conditional statement                
+                    // let x=9
+                    // if (x==10) {
+                    //     console.log("true");
+                    // }
+                    // else{
+                    //     console.log("false");
+                    // }
+
+//Ternary Operator
+                    // var x=6;
+                    // var y=(x>15)? "cold drink" :"juice"
+                    // console.log(y);
+
+//Switch Case
+     
+                    // var x=3;
+
+                    // switch (x) {
+                    //     case 1:
+                    //        console.log("hajrat"); 
+                    //         break;
+                    //     case 2:
+                    //     console.log("sarwar");    
+                    //         break;    
+                    //     case 3:
+                    //     console.log("alam");    
+                    //         break;
+                    //     default:
+                    //     console.log("this is default value");
+                    //         break;
+                    // }
+    
+//Sort Array
+                    //let arr=["sonu","monu","titu","pillu"]
+                        // console.log(arr);
+                        // console.log(arr.sort());
+                    // correct Method                    
+                        //let arr=[1,4,8,4,1,5,7,5,3,57,9]
+                        //  let brr=arr.sort()
+                        //     console.log(brr);
+                        //     arr.sort((a,b)=>{
+                        //         return a-b;
+                        //     })
+                        //     console.log(arr);
+// push method in array ==> push method will use to add element in last of array
+                        // console.log(arr);
+                        // arr.push("sarwar")
+                        // console.log(arr);
+// pop method in array will remove elemet in last and also return deleted element
+                 
+                        //   let pop1=arr.pop();
+                        //   console.log(arr); 
+                        // console.log(pop1); // pop returing deleted elemet also
+
+// Shift & unshift
+                        //unshift is use to add many element in starting of array
+                        // arr.unshift("banana", "sarwar")
+                        // console.log(arr);
+// shift is use to remove elemet from starting
+                        // arr.shift("sarwar")
+                        // console.log(arr);
+
+// Replace
+                        // let str="this is sarwar"
+                        // var replaced= str.replace("sarwar","repalace from sarwar")
+                        // console.log(replaced);
+
+
+// CharAt
+    
+                        // let arr="sarwar"
+                        // let neww=arr.charAt(2);
+                        // console.log(neww);
+           
+// IndexOf      // first occurance of substring                
+                        // var str= "this is a q q string"
+                        // var position=str.indexOf("q")
+                        // console.log(position);
+                        // last occurance of substring 
+                        // var occ=str.lastIndexOf("q")
+                        // console.log(occ);               
+// fill method
+
+                        // let arr=[1,4,5,7,8,5] 
+                        // let brr=arr.fill(0,2,4) // (value which will changed, starting of index, end)
+                        // console.log(brr);       
+ // Splice method use to delete and insert element in array
+                        // let arr=[2,3,4,6,7,8,9]
+                        // let brr=arr.splice(1,3) // starting of index, till delete index
+                        // console.log(brr); // 3,4,6 is available other deleted
+                
+
+// Loop 
+
+        // for loop
+
+                        // let i=0;
+                        // for(i=1;i<=10;i++){
+                        //     console.log(i);
+                        // }
+
+    
+
+                        // let frnd=["sarwar","srwr","alam","md"];
+        // for each loop
+
+                        // frnd.forEach(function (str) {
+                        // console.log(str);
+                                //})
+                        //or
+                                // frnd.forEach(str=>console.log(str))
+                        //or
+                                // const sarwar=[
+                        //     {name:"sarwar", age:"27", gender:"male"},
+                        //     {name:"alam", age:"28", gender:"female"},
+                        //     {name:"Md", age:"29", gender:"male"},
+                        //     {name:"srwr", age:"30", gender:"male"}
+                        // ]
+                        // sarwar.forEach(function (str){
+                        //     console.log(str.name);
+                        // })
+                        // sarwar.forEach(str=>console.log(str.name))
+        // for off       
+                        // for (str of frnd){
+                        // console.log(str);
+                        //      }
+
+        // for in loop   ==> for in loop mostly used in object
+
+                        // for(let index in frnd){
+                        //     console.log(frnd[index]);
+                        // }
+         
+        // while loop
+                        // let i=0;
+                        // while (i<10) {
+                        //     console.log(i);
+                        //     i++;
+                        // }
+        // Do while loop  run atleast one
+                        // let i=0;
+                        // do{
+                        //     console.log(i);
+                        //     i++;
+                        // }      
+                        // while(i<5)             
+
+// Array Destructuring
+
+                        //let var1=["mango", "banana" ,"apple","juice","coconut"]
+                        // let [myvar1,myvar2]=var1
+                        // console.log(myvar1);
+                        // console.log(myvar2);
+
+                        // let [mynew1, ,mynew2]=var1  // if you want to store 3rd element in mynew2, it mean to skip second element, just need to use space between two veriable
+                        // console.log(mynew1);
+                        // console.log(mynew2);
+
+        // To create new array from other array
+        // 1st method using slice method
+                        
+                        // let newarr=var1.slice(1,4)
+                        // console.log(newarr);
+        
+// Spread operator
+
+                        // let [myvar1,myvar2,...newArray]=var1
+                        // console.log(myvar1);
+                        // console.log(myvar2);
+                        // console.log(newArray);
+
+// Object destructuring
+
+                        // let person={
+                        //     name:"sarwar",
+                        //     age:"26",
+                        //     gender:"Male"
+                        // }
+                        // console.log(person.name);
+                        // console.log(person.age);
+                        // console.log(person.gender);
+
+                        // person.mood="ok"
+                        // console.log(person.mood);
+    
+// function
+
+                        // function sum (num){
+                        //     if (num<10) {
+                        //         return true;
+                        //     }
+                        //     return false
+                        // }
+                        // console.log(sum(11))
+
+// Rest Parameter
+    
+                            // function sum (...sum1){
+                            //     console.log(sum1);
+                            // }
+                            // sum(1,2,3,5,6,7) // you can add as mamy as value
+            // function sum(...total){
+            // let result=0
+            // for(let i=0;i<total.length;i++){
+            // result+=total[i];}
+            // console.log(result);
+            // }            
+            // sum(2,8)
+
+       
+                
+
+// Map function  
+         //map function is use to transform an array by creating new array.
+
+                // const arr=[3,5,3,2,5] 
+                // function double(number){
+                //     return number*2;
+                // }      
+                // const output=arr.map(double);
+                // console.log(output) ;
+            
+            //or
+                // const sarwar=[
+                //     {name:"sarwar", age:"27", gender:"male"},
+                //     {name:"alam", age:"28", gender:"female"},
+                //     {name:"Md", age:"29", gender:"male"},
+                //     {name:"srwr", age:"30", gender:"male"}
+                // ]     
+                // const username=sarwar.map((str)=>{
+                // return str.name;})
+                // console.log(username); 
+// filter
+        // it create a new array by removing element that not belongs to.
+
+
+                // const arr=[3,5,3,2,5] 
+                // function double(number){
+                //     return number>4;  // only change return condition and .filter
+                // }      
+                // const output=arr.filter(double);
+                // console.log(output) ;
+
+// Reduce 
+        // it takes all element in an array and reduce them into a single value
+
+                // const arr=[3,5,3,2,5]
+                // const output=arr.reduce(function (acc,curr){
+                //  acc=acc*curr;
+                //  return acc;},0)
+                // console.log(output);
+
+//  delete Unique or duplicate value
+
+                    // let arr=[1,2,3,4,2,2,5,6,8]
+                    // let brr= new Set(arr)
+                    // console.log(brr);
+
+         
+
+// function currying using bind method
+         
+                    // let sum=function (x,y){
+                    //     console.log(x*y);
+                    // }
+                    // let sum1=sum.bind(this,2)
+                    // sum1(3);
+
+// This Keyword
+         
+                //  const sarwar={
+                //         name:"Sarwar",
+                //         age:"34",
+                //         gender:"male",
+                //         sum:function (){
+                                
+                //                 console.log(this);
+                //         }
+                // }
+                // sarwar.sum();
+
+// Shadowing
+                //  var a=100;
+                //  {var a=10}
+                //  console.log(a);       
+
+// Deep copy
+         
+                // let details ={
+                //         name:"Sarwar",
+                //         age:"27",
+                //         branch:"Ec"
+                // }
+                // var details_deep_Copy ={
+                //         name:details.name,
+                //         age:details.age,
+                //         branch:details.branch
+                // }
+
+// Shallow copy
+        
+                // var emp_details={name:"sarwar",
+                //                 age:"26",
+                //                 branch:"EC"}
+                // var emp_details_shallowcopy=emp_details;                
+
+// IIFE
+                    // (function(){
+                    //     var name="sarwar"
+                    //     console.log("my name is " + name);
+                    // })()
+        
+         
---
+++ Topic.js
@@ -0,0 +1,301 @@
+// Hoisting
+
+                        // sum(3,5);   
+                        // function sum(a,b){
+                        //      sum=a+b;
+                        //     console.log(sum);
+                        // }
+    
+// Clouser
+                        // const outer_fun=(a)=>{
+                        //     let b=10;
+                        // const inner_fun=()=>{
+                        //     let sum=a+b;
+                        //     console.log("the sum of a & b is : " + sum);
+                        // }    
+                        // inner_fun();
+                        // }
+                        //  outer_fun(8);
+
+                        // function hello(x) {
+                        //     const a = "varA";
+                        //     const b = "varB";
+                        //     return function () {
+                        //         console.log(a, b, x);
+                        //     }
+                        // }
+                        // const ans = hello("arg");
+                        // ans();
+
+ // Higher order function
+
+                        // const x=()=>
+                        // console.log("Hello, I am first order");
+                        // const y=>(return x)=> return x();
+                        // y(x);   
+
+                        // Call Back function
+    
+                        // function fun1 (name){
+                        //     console.log("Hello " + name);}
+                        // function fun2 (call_back){
+                        //     let name=prompt("Your name");
+                        //     call_back (name);
+                        // }    
+                        // fun2(fun1)
+                        
+                            //or
+                        //     function fun1 (){
+                        //     console.log("Hello ")}
+                        // function fun2 (call_back){
+                        //     console.log("hi call back function");
+                        //     call_back ();
+                        // }    
+                        // fun2(fun1)
+
+// Promises
+
+                        // function prom (complete){
+                        // return new Promise(function(resolve, reject){
+                        //     console.log("fetching data, Pls wait");
+                        //     setTimeout(()=>{
+                        //         if(complete){
+                        //         resolve("i am sucessful");
+                        //     }else{
+                        //     reject("I am failed");}},3000)});}
+
+                        // let onfulfillment=(result)=>{
+                        //     console.log(result);}
+                            
+                        // let onRejection=(error)=>{
+                        //     console.log(error);}
+                        // prom(true).then(onfulfillment).catch (onRejection);  
+
+// API
+                        //     fetch ("https://mocki.io/v1/d4867d8b-b5d5-4a48-a4ab-79131b5809b8").then((response)=>response.json())
+                        //     .then((data)=>{console.log(data);
+                        //     for(var x in data){
+                        //         document.write(`${data[x].name}- <br> <br> ${data[x].city} <br>`);}})
+                        //    .catch(()=>document.write("can not fetch data")); 
+            
+//API in table
+          
+                        // <!DOCTYPE html>
+                        // <html lang="en">
+                        // <head>
+                        //     <meta charset="UTF-8">
+                        //     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                        //     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        //     <title>Document</title>
+                            
+                        // </head>
+                        // <body>
+                        //     <div class="container">
+                        //         <table class="table" border="1">
+                        //             <thead>
+                        //             <tr>
+                        //                 <th>ID</th>
+                        //                 <th>Employee Name</th>
+                        //                 <th>Salary</th> 
+                        //             </tr>
+                        //             </thead>
+                        //             <tbody id="data">
+                                
+                        //             </tbody>
+                        //         </table>
+                        //         </div>
+                        //         <script>
+                        //         fetch("http://dummy.restapiexample.com/api/v1/employees").then(
+                        //     res => {
+                        //     res.json().then(
+                        //         data => {
+                        //         console.log(data.data);
+                        //         if (data.data.length > 0) {
+                        
+                        //             var temp = "";
+                        //             data.data.forEach((itemData) => {
+                        //             temp += "<tr>";
+                        //             temp += "<td>" + itemData.id + "</td>";
+                        //             temp += "<td>" + itemData.employee_name + "</td>";
+                        //             temp += "<td>" + itemData.employee_salary + "</td></tr>";
+                        //             });
+                        //             document.getElementById('data').innerHTML = temp;
+                        //         }
+                        //         }
+                        //     )
+                        //     }
+                        // )
+                        //         </script>
+                        // </body>
+                        // </html>
+
+// Make a table and fill it using prompt
+                          
+                        // <table id="myTable" border="1">
+                        // <tr>
+                        // <td>Row1 cell1</td>
+                        // <td>Row1 cell2</td>
+                        // </tr>
+                        // <tr>
+                        // <td>Row2 cell1</td>
+                        // <td>Row2 cell2</td>
+                        // </tr>
+                        // <tr>
+                        // <td>Row3 cell1</td>
+                        // <td>Row3 cell2</td>
+                        // </tr>
+                        // </table>
+                        // <form>
+                        // <input type="button" value="Change content">
+                        // </form>
+
+                        // <script>
+                            
+                        // let button = document.querySelector('input');
+                        // button.addEventListener('click', changeCell);
+
+                        // function changeCell() {
+                        // let row = window.prompt('enter row: ') - 1 ;
+                        // let column = window.prompt('enter column: ') - 1;
+                        // let newValue = window.prompt('enter new value');
+                        // document.querySelectorAll('tr')[row].children[column].textContent = newValue;
+                        // }
+                        // </script>
+  
+
+
+// call,bind & Apply
+                        // let name1={
+                        //     first_name:"Sarwar",
+                        //     last_name:"alam"
+                        // }
+                        // let printobj=function(age,city){
+                        //     console.log(this.first_name + " "+ this.last_name,age,city);
+                        // }
+                        // let name2={
+                        //     first_name:"Md Sarwar",
+                        //     last_name:"Ali"
+                        // }
+// Call            
+                        //printobj.call(name1,"27","mumbai");
+                    
+        // Apply
+                         //  printobj.apply(name2,["27","mumbai"])
+        // Bind
+                        // let mybind=printobj.bind(name2,"mumbai","25");
+                        // mybind(); // need to call function in bind                    
+
+ // setTimeout() and setTime Interval
+         
+                        // function sarwar(name,roll_no){
+                        //         console.log("hello " + name +" "+roll_no);}
+                        //         time_out=setTimeout(sarwar,5000, "alam", "5");
+                        //         console.log(time_out); // it will provide setTime out Id
+                        // interval_Id= setInterval(sarwar,3000,"Sarwar","5"); // print same o/p contineously
+              // clearInterval(interval_Id) // when you want to clear interval means stop interval time
+
+
+// Event Bubbling
+                        // <title>Document</title>
+                        // <style>
+                        //     body{
+                        //         background-color: aqua;
+                        //         padding: 100px;
+                        //     }
+                        //     #myDiv{
+                        //         padding: 50px;
+                        //         background-color: blue;
+                        //     }
+                        // </style>
+                        // </head>
+                        // <body>
+                        // <div id="myDiv">
+                        // <button id="btn">Click me</button> </div>
+                        // <script>
+                        //     let d=document.getElementById("myDiv");
+                        //     let b=document.getElementById("btn");
+                        //     b.addEventListener('click',btnclicked,true);
+                        //     d.addEventListener('click',divclicked,true);
+                        //     document.body.addEventListener('click',bodyclicked);
+                        //     function btnclicked(){
+                        //         console.log("button clicked: ")
+                        //         event.stopPropagation()
+                        //     }
+
+                        //     function divclicked(){
+                        //         console.log("div clicked: ")
+                        //     }
+
+                        //     function bodyclicked(){
+                        //         console.log("body clicked: ")
+                        //     }
+
+
+// Iterator
+
+                        // function fruit_Iterator(value){
+                        //     let next_Index=0;
+                        //     return{
+                        //         next:function(){
+                        //             if(next_Index<value.length){
+                        //                 return{
+                        //                     values:value[next_Index++],
+                        //                     done:false }}
+                        //                 else{return{done:true}}}}}
+                        // const myArray=["Apple","grapes","Banana"];
+                        // console.log("My Array is : ",myArray);
+
+                        // // Use Iterator
+                        // const fruits=fruit_Iterator(myArray);
+                        // console.log(fruits.next());
+                        // console.log(fruits.next);
+
+
+                   
+// Iterators
+
+                        // function fruitsIterator(values) {
+                        //     let nextIndex = 0;
+                        //     // we will return an object
+                        //     return {
+                        //         next: function () {
+                        //             if (nextIndex < values.length) {
+                        //                 // We will return below object
+                        //                 return {
+                        //                     value: values[nextIndex++],
+                        //                     done: false
+                        //                 }
+                        //             }
+                        //             else {
+                        //                 // We will return below object with only done
+                        //                 return {
+                        //                     done: true
+                        //                 }
+                        //             }
+                        //         }
+                        //     }
+                        // }
+
+
+                        // const myArray = ['Apples', 'Grapes', 'Oranges', 'Bhindi'];
+                        // console.log("My array is ", myArray)
+
+                        // // Using the iterator
+                        // const fruits = fruitsIterator(myArray);
+                        // console.log(fruits.next().value)
+                        // console.log(fruits.next().value)
+                        // console.log(fruits.next().value)
+                        // console.log(fruits.next().value)
+                        // console.log(fruits.next().value)
+
+
+// Generator        Iterator and generator is not clear
+                        // function numberGen(){
+                        //     let i=0;
+                        //     yield 1;
+                        //     yield 2;
+                        //     while(true){
+                        //         yield i++;}}
+                        //     const gen=numberGen();
+                        //     console.log(gen.next().value);
+                        //     console.log(gen.next().value);                        
